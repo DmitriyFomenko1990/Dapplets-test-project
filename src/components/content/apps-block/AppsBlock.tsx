b85e1b6d1a202ac5036ab532d6d558b57dc3687d
Dmitriy <fomenkodmitriy1990@gmail.com>
2021-07-26 20:58:07 +0300

    add Drag & drop

--- src/components/content/apps-block/AppsBlock.tsx
+++ src/components/content/apps-block/AppsBlock.tsx
@@ -6,15 +6,36 @@ import {useDispatch} from "react-redux";
 import {fetchDapplets, fetchDappletsForPagination} from "../../../store/action-creator";
 import {useTypedSelector} from "../../../store/redux/combineReducers";
 import {DappletType} from "../../../store/redux/dappletsReducerTypes";
+import {
+    DragDropContext,
+    Draggable,
+    Droppable,
+    DroppableProvided,
+    DropResult,
+    DraggableProvided} from 'react-beautiful-dnd';
+
+// TODO: d&d refactor
 
 interface AppsBlockProps  {
     isOpenSideBar: boolean;
-};
+}
+
+interface Item {
+    id: string;
+    content: string;
+}
+interface IAppState {
+    items: Item[];
+    selected: Item[];
+}
+
+
 
 const AppsBlock: React.FC<AppsBlockProps> = ({isOpenSideBar}) => {
     const dispatch = useDispatch()
     const [page, setPage] = useState(0)
     const [fetching, setFetching] = useState(false)
+    const [DnDState, setDnDState] = useState<any>();
 
     useEffect(() => {
             dispatch(fetchDapplets(page,20,'privacy','DESC'))
@@ -46,17 +67,66 @@ const AppsBlock: React.FC<AppsBlockProps> = ({isOpenSideBar}) => {
     };
     const dappletsState: DappletType[] = useTypedSelector(state => state.dappletReducer.dapplets);
 
-     const dapplets = dappletsState.map(dap => {
-        return <Application dapplet={dap} isOpenSideBar={isOpenSideBar}  key={dap.id} />
-    });
+
+    const reorder = (list: Item[], startIndex: number, endIndex: number):Item[] => {
+        const result = [...list];
+        const [removed] = result.splice(startIndex, 1);
+        result.splice(endIndex, 0, removed);
+
+        return result;
+    };
+
+    function onDragEnd(result: DropResult) {
+        if (!result.destination) {
+            return;
+        }
+
+        if (result.destination.index === result.source.index) {
+            return;
+        }
+
+        const items: Item[] = reorder(
+            DnDState.quotes,
+            result.source.index,
+            result.destination.index
+        );
+
+        setDnDState({ items });
+    }
+    //  const dapplets = dappletsState.map(dap => {
+    //     return <Application dapplet={dap} isOpenSideBar={isOpenSideBar}  key={dap.id} />
+    // });
     return (
         <div className={style.wrapper} onScroll={onHandleScroll}>
-            <div className={style.dapplets}>
-                {dapplets}
-            </div>
+            <DragDropContext onDragEnd={onDragEnd}>
+                <Droppable droppableId='dapplets'>
+                    { (provided: DroppableProvided)  => (
+                        <div className={style.dapplets} ref={provided.innerRef}
+                                {...provided.droppableProps} >
+                            {
+                                dappletsState.map((dap, index) => (
+                                        <Draggable key={index} draggableId={dap.id} index={index}>
+                                            {(providedDraggable:DraggableProvided) => (
+                                                <div>
+                                                    <div ref={providedDraggable.innerRef}
+                                                        {...providedDraggable.draggableProps}
+                                                        {...providedDraggable.dragHandleProps}>
+                                                        <Application dapplet={dap} isOpenSideBar={isOpenSideBar}/>
+                                                    </div>
+                                                    {provided.placeholder}
+                                                </div>
+                                            )}
+                                        </Draggable>
+                                    )
+                                )
+                            }
+                        </div>
+                    )}
+                </Droppable>
+            </DragDropContext>
             { page < 11
                 ? <Loader isFetching={fetching}/>
-                : <div></div>
+                : <div/>
             }
         </div>
     );
